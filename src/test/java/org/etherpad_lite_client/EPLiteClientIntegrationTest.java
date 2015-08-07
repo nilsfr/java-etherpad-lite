2191a8550db5ca4e2147a14aaf0bc6284b0aafcf
Nils Fredrik Gjerull <nils@gjerull.net>
2015-08-07 13:12:17 +0200

    Improve maven config.
    Do not run EPLiteClientIntegrationTest as default, as it require
    a running Ethepad instance with a specific api key.

---
+++ src/test/java/org/etherpad_lite_client/EPLiteClientIntegrationTest.java
@@ -0,0 +1,51 @@
+package org.etherpad_lite_client;
+
+import java.util.List;
+import java.util.HashMap;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Integration test for simple App.
+ */
+public class EPLiteClientIntegrationTest {
+    private EPLiteClient client;
+
+    /**
+     * Useless testing as it depends on a specific API key
+     */
+    @Before
+    public void setUp() throws Exception {
+        this.client = new EPLiteClient("http://localhost:9001", "K8OF91QMQYUvrNu3e9rJ7FnnVgaB3m9q");
+
+    }
+
+    @Test
+    public void create_pad_set_and_get_content() {
+        client.createPad("java_test_pad");
+        client.setText("java_test_pad", "foo!!!");
+        HashMap pad = client.getText("java_test_pad");
+        client.deletePad("java_test_pad");
+
+        String text = pad.get("text").toString();
+        assertEquals("foo!!!\n", text);
+    }
+
+    @Test
+    public void create_pads_and_list_them() {
+        client.createPad("java_test_pad_1");
+        client.createPad("java_test_pad_2");
+        HashMap result = client.listAllPads();
+        client.deletePad("java_test_pad_1");
+        client.deletePad("java_test_pad_2");
+
+        List padIDs = (List) result.get("padIDs");
+        assertTrue(padIDs.size() > 2);
+        assertTrue(padIDs.contains("java_test_pad_1"));
+        assertTrue(padIDs.contains("java_test_pad_2"));
+    }
+}
